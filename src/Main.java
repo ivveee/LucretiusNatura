07c658862ad7cb74b17033b8da62d2c2cce3e14f
ivveee <ivee@list.ru>
2013-09-01 17:26:22 +0400

    First

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//package src;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        luc.main("luc");
+    }
+}
